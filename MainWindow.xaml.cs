a97ee2a1faecf9d480c5ec18618a2203d18365fc
p288004 <p288004@MD-T103-611611>
2025-07-24 09:54:07 +0800

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+using System.Windows;
+
+namespace CalcWPFApp
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Multiply_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                double num1 = Convert.ToDouble(Input1.Text);
+                double num2 = Convert.ToDouble(Input2.Text);
+                double result = num1 * num2;
+
+                ResultText.Text = $"Result: {result}";
+
+                File.WriteAllText("calc.txt", $"Multiplication Result: {result}");
+            }
+            catch
+            {
+                ResultText.Text = "Please enter valid numbers!";
+            }
+        }
+    }
+}
