59cc2e89007d1f71339399ef0fc69318a7eaa65f
p288004 <p288004@MD-T103-611611>
2025-07-24 10:01:19 +0800

    fixed

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,33 +1,58 @@
 ﻿using System;
+
 using System.IO;
+
 using System.Windows;
 
 namespace CalcWPFApp
+
 {
+
     public partial class MainWindow : Window
+
     {
+
         public MainWindow()
+
         {
+
             InitializeComponent();
+
         }
 
         private void Multiply_Click(object sender, RoutedEventArgs e)
+
         {
+
             try
+
             {
+
                 double num1 = Convert.ToDouble(Input1.Text);
+
                 double num2 = Convert.ToDouble(Input2.Text);
+
                 double result = num1 * num2;
 
                 ResultText.Text = $"Result: {result}";
+                // Save the result to a file
 
                 string path = @"C:\Users\p288004\Desktop\calc.txt";
-                File.WriteAllText("calc.txt", $"Multiplication Result: {result}");
+
+                File.WriteAllText(path, $"Multiplication Result: {result}");
+
             }
+
             catch
+
             {
+
                 ResultText.Text = "Please enter valid numbers!";
+
             }
+
         }
+
     }
+
 }
